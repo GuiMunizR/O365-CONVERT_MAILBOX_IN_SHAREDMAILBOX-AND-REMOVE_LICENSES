d1b050561c0ca37d4fd795090000c9bfc5e1c6d4
GuiMunizR <guilherme.muniz@outlook.com.br>
2022-04-22 02:11:22 -0300

    Commit 20220422-0211

--- SINGLEUSER-CONVERT_MAILBOX_IN_SHAREDMAILBOX-AND-REMOVE_LICENSES.ps1
+++ SINGLEUSER-CONVERT_MAILBOX_IN_SHAREDMAILBOX-AND-REMOVE_LICENSES.ps1
@@ -1,4 +1,4 @@
 #Created by: Warley Roverse (linkedin.com/in/warleyroverse)
-#Edited and revised by: Guilherme Muniz (linkedin.com/in/guilhermemunizr)
-##NAME: MULTIUSER-CONVERT_MAILBOX_IN_SHAREDMAILBOX-AND-REMOVE_LICENSES
-###OBJETIVE: 
+##Edited and revised by: Guilherme Muniz (linkedin.com/in/guilhermemunizr)
+###NAME: MULTIUSER-CONVERT_MAILBOX_IN_SHAREDMAILBOX-AND-REMOVE_LICENSES
+####OBJETIVE: 
